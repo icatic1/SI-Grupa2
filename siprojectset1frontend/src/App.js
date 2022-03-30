3ccea59d18531f2aa4b6e4d0b07e204f882b49be
fcosic1 <fcosic1@etf.unsa.ba>
2022-03-30 18:07:33 +0200

    NavigationHeader i Home
    Dodan NavigationHeader i Home komponenta

--- siprojectset1frontend/src/App.js
+++ siprojectset1frontend/src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Login from './components/Login'
+import NavigationHeader from './components/NavigationHeader'
+import Home from './components/Home'
 
 import './App.css';
 function App() {
     return (
         <Router>
+            <NavigationHeader />
             <Routes>
+                <Route exact path="/Home" element={<Home />} />
                 <Route exact path='/' element={<Login />} />
             </Routes>
         </Router>)
---
+++ siprojectset1frontend/src/components/NavigationHeader.js
@@ -0,0 +1,29 @@
+import { Container, Navbar, ListGroup } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+
+
+
+function NavigationHeader() {
+
+
+    return (
+        <>
+
+            <Navbar className="fluid navbar navbar-expand-lg navbar-light bg-light mx-auto">
+                <Container className="mx-auto">
+                    <ListGroup className="navbar-nav mr-auto">
+                        <ListGroup.Item><Link to={'/Home'} className="nav-link"> Home </Link></ListGroup.Item>
+                        <ListGroup.Item><Link to={'/'} className="nav-link"> Add user </Link></ListGroup.Item>
+                        <ListGroup.Item><Link to={'/'} className="nav-link">Users</Link></ListGroup.Item>
+                        <ListGroup.Item><Link to={'/'} className="nav-link">Logout</Link></ListGroup.Item>
+                    </ListGroup>
+
+                </Container >
+            </Navbar >
+
+        </>
+    )
+}
+
+export default NavigationHeader
