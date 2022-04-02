486562990ee21b72658cbf9b02c171ece56403b3
Irvin Catic <icatic1@etf.unsa.ba>
2022-04-02 12:52:34 +0200

    Refactoring verzije

--- SIProjectSet1/UserService/IMailService.cs
+++ SIProjectSet1/UserService/IMailService.cs
@@ -26,52 +26,16 @@ namespace SIProjectSet1.UserService
         public async Task SendNewMail(PasswordRequest request)
         {
 
-            /* MailMessage mes = new MailMessage(_mailSettings.Mail,request.ToEmail);
-             mes.Subject = "SnapShot - Reset password";
-             mes.Body = "This is a sample message using SMTP authentication";
-
-
-             string FilePath = Directory.GetCurrentDirectory() + "\\MailAddOn\\front.html";
-             var msgBody = new StringBuilder(File.ReadAllText(FilePath));
-             //string currentURL = Dns.GetHostName();
-             string currentURL = "http://sigrupa4-001-site1.ctempurl.com";
-             msgBody.Replace("[username]", request.UserName);
-             msgBody.Replace("[email]", request.ToEmail);
-             msgBody.Replace("[token]", currentURL + "/ChangePass/" + request.Token);
-
-             mes.IsBodyHtml = true;
-             mes.Body = msgBody.ToString();
-             SmtpClient sc = new SmtpClient();
-
-             sc.Host = _mailSettings.Host;
-             sc.Port = 587;
-             sc.UseDefaultCredentials = false;
-             sc.Credentials = new System.Net.NetworkCredential(_mailSettings.Mail,_mailSettings.Password);
-             sc.EnableSsl = true;
-             sc.Send(mes);*/
-            //string newStuf = "";
 
             MailMessage Message = new MailMessage("snapshotagent@gmail.com", request.ToEmail);
             
             Message.Subject = "SnapShot - Reset password";
-            //Message.Body = "This is a sample message using SMTP authentication";
-            //Message.IsBodyHtml = false;
-            /* string FilePath = Directory.GetCurrentDirectory() + "\\MailAddOn\\front.html";
-             var msgBody = new StringBuilder(File.ReadAllText(FilePath));
-
-             string currentURL = "http://sigrupa4-001-site1.ctempurl.com";
-             msgBody.Replace("[username]", request.UserName);
-             msgBody.Replace("[email]", request.ToEmail);
-             msgBody.Replace("[token]", currentURL + "/ChangePass/" + request.Token);
-
-             Message.IsBodyHtml = true;
-             Message.Body = msgBody.ToString();*/
+            
             string currentURL = "http://sigrupa4-001-site1.ctempurl.com";
             Message.Body = "Reset Password link:  " + currentURL + "/ChangePass/" + request.Token;
 
             SmtpClient smtp = new SmtpClient("smtp.gmail.com");
                 
-                    // smtp.Host = "smpt.gmail.com";
             smtp.EnableSsl = true;
 
             NetworkCredential cred = new NetworkCredential("snapshotagent@gmail.com", "123123SI");
