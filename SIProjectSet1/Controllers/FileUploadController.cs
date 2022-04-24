4e88483772414e7fb5182e878d7344142c1a8936
ahrnjic3 <72994482+ahrnjic3@users.noreply.github.com>
2022-04-24 12:04:45 +0200

    read files route added

---
+++ SIProjectSet1/Controllers/FileUploadController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace SIProjectSet1.Controllers
+{
+
+
+   
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FileUploadController : ControllerBase
+    {
+        private readonly ILogger<FileUploadController> _logger;
+
+        public FileUploadController(ILogger<FileUploadController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("ReadLargeFile/{FileName}")]
+        public async Task<IActionResult> ReadLargeFile(String FileName)
+        {
+            var files = Directory.GetFiles(Path.GetDirectoryName(Path.GetFullPath(FileName)));
+            return Ok(files);
+        }
+
+        [HttpGet]
+        [Route("GetFilesByMac/{MacAddress}")]
+        public async Task<IActionResult> GetFilesByMac(String MacAddress)
+        {
+            var files = Directory.GetFiles(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", MacAddress));
+            return Ok(files);
+        }
+
+    }
+}
