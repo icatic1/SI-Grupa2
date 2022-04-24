50cac49ffa97cf8b016e15d7dd10317230edf17c
Nedim Badžak <nbadzak1@etf.unsa.ba>
2022-04-24 17:40:37 +0200

    Change to real database

--- SIProjectSet1/Migrations/20220423122705_blabla.cs
+++ SIProjectSet1/Migrations/20220423122705_blabla.cs
@@ -8,116 +8,12 @@ namespace SIProjectSet1.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                name: "FTAs",
-                columns: table => new
-                {
-                    Id = table.Column<long>(type: "bigint", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    UserId = table.Column<long>(type: "bigint", nullable: false),
-                    token = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    isActivated = table.Column<bool>(type: "bit", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_FTAs", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "JsonConfigurations",
-                columns: table => new
-                {
-                    MacAddress = table.Column<string>(type: "nvarchar(450)", nullable: false),
-                    Configuration = table.Column<string>(type: "nvarchar(max)", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_JsonConfigurations", x => x.MacAddress);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "Licences",
-                columns: table => new
-                {
-                    MacAddress = table.Column<string>(type: "nvarchar(450)", nullable: false),
-                    TerminalID = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Licenced = table.Column<bool>(type: "bit", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Licences", x => x.MacAddress);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "PassTokens",
-                columns: table => new
-                {
-                    Id = table.Column<long>(type: "bigint", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Email = table.Column<string>(type: "varchar(250)", nullable: false),
-                    ResetToken = table.Column<string>(type: "varchar(250)", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_PassTokens", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "Roles",
-                columns: table => new
-                {
-                    Id = table.Column<long>(type: "bigint", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Roles", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "SecurityQuestions",
-                columns: table => new
-                {
-                    Id = table.Column<long>(type: "bigint", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    UserId = table.Column<long>(type: "bigint", nullable: false),
-                    Question = table.Column<string>(type: "varchar(250)", nullable: false),
-                    Answer = table.Column<string>(type: "varchar(250)", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_SecurityQuestions", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "Users",
-                columns: table => new
-                {
-                    Id = table.Column<long>(type: "bigint", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Email = table.Column<string>(type: "varchar(250)", nullable: false),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    DeletedStatus = table.Column<bool>(type: "bit", nullable: false),
-                    RoleId = table.Column<long>(type: "bigint", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Users", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Users_Roles_RoleId",
-                        column: x => x.RoleId,
-                        principalTable: "Roles",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Users_RoleId",
-                table: "Users",
-                column: "RoleId");
+            migrationBuilder.AddColumn<bool>(
+                name: "TerminalID",
+                table: "Licences",
+                type: "nvarchar(max)",
+                nullable: true,
+                defaultValue: false);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
