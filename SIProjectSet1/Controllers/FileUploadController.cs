0473f3881e0e407cee89186e64f64c81b943cf56
Nedim Badžak <nbadzak1@etf.unsa.ba>
2022-05-10 21:06:35 +0200

    Start adding routes for live streaming

--- SIProjectSet1/Controllers/FileUploadController.cs
+++ SIProjectSet1/Controllers/FileUploadController.cs
@@ -7,6 +7,9 @@ using SIProjectSet1.FilesService;
 using SIProjectSet1.ViewModels;
 using System.IO;
 using System.IO.Compression;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -26,6 +29,8 @@ namespace SIProjectSet1.Controllers
     {
         private readonly ILogger<FileUploadController> _logger;
         private readonly IFilesService _filesService;
+        private static byte[]? imageBase64ByteArray;
+        private static Queue<byte[]> queue = new Queue<byte[]>();
 
         public FileUploadController(ILogger<FileUploadController> logger, IFilesService filesService)
         {
@@ -144,6 +149,94 @@ namespace SIProjectSet1.Controllers
             return BadRequest("No files data in the request.");
         }
 
+
+        /// <summary>
+        /// Action for live streaming large files
+        /// </summary>
+        /// <remarks>
+        /// Request to this action will not trigger any model binding or model validation,
+        /// because this is a no-argument action
+        /// </remarks>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("StreamLargeFile")]
+        public async Task<IActionResult> LiveStreamLargeFile()
+        {
+            var request = HttpContext.Request;
+            // validation of Content-Type
+            // 1. first, it must be a form-data request
+            // 2. a boundary should be found in the Content-Type
+            if (!request.HasFormContentType ||
+                !MediaTypeHeaderValue.TryParse(request.ContentType, out var mediaTypeHeader) ||
+                string.IsNullOrEmpty(mediaTypeHeader.Boundary.Value))
+            {
+                return new UnsupportedMediaTypeResult();
+            }
+            var reader = new MultipartReader(mediaTypeHeader.Boundary.Value, request.Body);
+            var section = await reader.ReadNextSectionAsync();
+
+            // This sample try to get the first file from request and save it
+            // Make changes according to your needs in actual use
+            while (section != null)
+            {
+                var hasContentDispositionHeader = ContentDispositionHeaderValue.TryParse(section.ContentDisposition,
+                    out var contentDisposition);
+
+                if (hasContentDispositionHeader && contentDisposition.DispositionType.Equals("form-data") &&
+                    !string.IsNullOrEmpty(contentDisposition.FileName.Value))
+                {
+                    // Don't trust any file name, file extension, and file data from the request unless you trust them completely
+                    // Otherwise, it is very likely to cause problems such as virus uploading, disk filling, etc
+                    // In short, it is necessary to restrict and verify the upload
+                    // Here, we just use the temporary folder and a random file name
+
+                    // Get the temporary folder, and combine a random file name with it
+                    var fileName = contentDisposition.Name.Value;
+                    //var MacAddress = contentDisposition.Name.Value;
+                    //var saveToPath = Path.Combine(Path.GetTempPath(), fileName);
+                    //Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "UserContent", fileName));
+                    //var saveToPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "UserContent", fileName, contentDisposition.FileName.Value);
+
+                    //using (var targetStream = System.IO.File.Create(saveToPath))
+                    //{
+                    //    await section.Body.CopyToAsync(targetStream);
+                    //}
+                    //////////
+                    ///
+
+
+                    //await section.Body.ReadAsync(imageBase64ByteArray, 0, (int)section.Body.Length);
+                    MemoryStream ms = new MemoryStream();
+                    await section.Body.CopyToAsync(ms);
+                    imageBase64ByteArray = ms.ToArray();
+                    queue.Enqueue(imageBase64ByteArray);
+                    while(queue.Count >= 150)
+                    {
+                        queue.Dequeue();
+                    }
+                    return Ok();
+
+                    //if (imageBase64ByteArray != null)
+                    //{
+                    //    var data = Encoding.UTF8.GetString(imageBase64ByteArray);
+                    //    data = data.Replace("data:image/png;base64,", "");
+                    //    byte[] imageData = Convert.FromBase64String(data);
+                    //    return Ok();
+                    //}
+                    //return BadRequest("Neki error bio");
+
+
+                }
+
+                section = await reader.ReadNextSectionAsync();
+            }
+
+            // If the code runs to this location, it means that no files have been saved
+            return BadRequest("No files data in the request.");
+        }
+
+
+
         /// <summary>
         /// Action for returning all files and folders for a given device
         /// </summary>
@@ -160,6 +253,22 @@ namespace SIProjectSet1.Controllers
             return Ok(entries);
         }
 
+
+        [HttpGet]
+        [Route("GetLiveFile")]
+        public async Task<IActionResult> GetLiveFile()
+        {
+            String? data = "";
+            //if (imageBase64ByteArray != null)
+            //{
+                //data = Encoding.UTF8.GetString(imageBase64ByteArray);
+                //data = data.Replace("data:image/png;base64,", "");
+                //byte[] imageData = Convert.FromBase64String(data);
+                return Ok(Convert.ToBase64String(queue.Dequeue()));
+            //}
+            //else return BadRequest("Errorcina");
+        }
+        
 
         //[HttpGet]
         //[Route("GetFilesByPathSorted/{path}")]
@@ -186,8 +295,54 @@ namespace SIProjectSet1.Controllers
         {
 
             var a = await _filesService.GetPathsSortedNew(path);
-            if(a != null) return Ok(a);
+            if (a != null) return Ok(a);
             return BadRequest(a);
+        }
+
+
+        [HttpGet]
+        [Route("Tester")]
+        public async Task<IActionResult> Tester()
+        {
+
+            string dirPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "UserContent", "Test");
+            if (!Directory.Exists(dirPath)) return null;
+            List<byte[]> lista = new List<byte[]>();
+            var imagesDir = Directory.GetFiles(dirPath);
+            foreach(var image in imagesDir)
+            {
+                try
+                {
+                    var memory = new MemoryStream();
+                    using (var stream = new FileStream(image, FileMode.Open))
+                    {
+                        await stream.CopyToAsync(memory);
+                        
+                    }
+                    byte[] array = memory.ToArray();
+                    lista.Add(array);
+                    memory.Position = 0;
+                    
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest("There is not a config file present for the provided device. ");
+                }
+            }
+
+            for (int i=0; i<2; i++)
+            {
+                for(int j=0; j<lista.Count; j++)
+                {
+                    queue.Enqueue(lista[j]);
+                }
+            }
+            while (queue.Count >= 150)
+            {
+                queue.Dequeue();
+            }
+            return Ok();
+            
         }
 
         [HttpPost]
---
+++ siprojectset1frontend/src/components/Live.js
@@ -0,0 +1,34 @@
+﻿////const [imgSrc, setImgSrc] = useState();
+
+
+////useEffect(() => {
+////    setInterval(() => {
+////        getImage().then(image => {
+////            setImgSrc("data:image/png;base64, " + image);
+////        });
+////    }, 32)
+
+////}, []);
+
+
+
+
+
+////async function getImage() {
+////    const response1 = await fetch('/api/FileUpload/GetLiveFile', {
+////        method: 'GET',
+////        headers: {
+////            'Content-Type': 'text/plain;charset=UTF-8'
+////        }
+////    });
+
+////    if (!response1.ok) return null
+////    const resp = await response1;
+////    const result = resp.text();
+
+////    return result;
+////}
+
+//return (
+//    <img src={imgSrc} />
+//    )
