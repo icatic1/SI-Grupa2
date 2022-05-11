16564f47beee0e91fe85c9a1d919096502934944
ahadzibajr1 <ahadzibajr1@etf.unsa.ba>
2022-05-11 19:41:44 +0200

    Add methods to start and end live camera preview

--- siprojectset1frontend/src/components/Live.js
+++ siprojectset1frontend/src/components/Live.js
@@ -1,5 +1,5 @@
-﻿import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+﻿import React, { useState, useEffect, useRef } from 'react'
+import { useParams, useLocation } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 
 const Live = () => {
@@ -7,24 +7,46 @@ const Live = () => {
     const { mac } = useParams()
     const { id } = useParams()
     const { num } = useParams()
+    
 
     useEffect(() => {
 
-
-        /* setInterval(() => {
-             getImage().then(image => {
-                 setImgSrc("data:image/png;base64, " + image);
-             });
-         }, 32)*/
+        if (startLive()) {
+            const interval = setInterval(() => {
+                getImage().then(image => {
+                    setImgSrc("data:image/png;base64, " + image);
+                });
+            }, 32)
+            return () => {
+                console.log(interval)
+                clearInterval(interval)
+                endLive()
+            }
+        }
+        
 
     }, []);
 
+    async function startLive() {
+        var res = await fetch('/api/FileUpload/ChangeStreamState/' + mac + '/1')
 
+        if (res.ok) {
+            console.log("Start live")
+            return true
+        }
+        return false
+    }
 
+    async function endLive() {
+        
+        await fetch('/api/FileUpload/ChangeStreamState/' + mac + '/0')
+        console.log("End live")
+        
+    }
 
 
     async function getImage() {
-        const response1 = await fetch('/api/FileUpload/GetLiveFile', {
+        const response1 = await fetch('/api/FileUpload/GetLiveFile/' + mac, {
             method: 'GET',
             headers: {
                 'Content-Type': 'text/plain;charset=UTF-8'
@@ -43,8 +65,8 @@ const Live = () => {
         <Container style={{ background: "rgba(0, 22, 56, 0.69)", padding: "5%", borderRadius: "20px", marginBottom: "20px" }}>
 
             <div style={{ background: "white", width: "35%", marginBottom: "0", borderTopLeftRadius: "30px", borderTopRightRadius: "30px", padding: "5px" }}>
-                <h1 style={{ color: "#035296", paddingLeft: "40px", marginBottom: "0", fontSize: "xx-large" }}>{id}</h1>
-                <h2 style={{ color: "#035296", paddingLeft: "50px", marginBottom: "0", fontSize: "x-large" }}>Camera {num}</h2>
+                <h1 style={{ color: "#035296", paddingLeft: "40px", marginBottom: "0", fontSize: "150%" }}>{id}</h1>
+                <h2 style={{ color: "#035296", paddingLeft: "50px", marginBottom: "0", fontSize: "130%" }}>Camera {num}</h2>
             </div>
 
             < img src={imgSrc} style={{ width: "100%", height: "100%", margin: "0", maxHeight: "500px", maxWidth: "960px", borderTopRightRadius: "10px", borderBottomLeftRadius: "10px", borderBottomRightRadius: "10px" }} />
